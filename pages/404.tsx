748858f319a4ca66a4e8eefbbacb562568d8f479
Nidolai <11526011+Nidolai@users.noreply.github.com>
2022-08-05 22:21:27 +0200

    Formatting

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,13 +4,13 @@ import Container from "../components/Container";
 const Custom404: NextPage = () => {
     return (
         <Container
-            subTitle = "404"
+            subTitle="404"
         >
             <div className="flex flex-col items-start justify-center max-w-2xl mx-auto">
                 <h1 className="mb-3 text-3xl font-bold tracking-tight md:text-5xl dark:text-white mt-5">404</h1>
                 <p className="dark:text-gray-400">
                     The page you are looking for does not exist.
-                    <br/>
+                    <br />
                     Try to navigate to another page using the navigation down below.
                 </p>
             </div>

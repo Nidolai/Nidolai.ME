49c8e77f10351112f4d58c5d5373b75e7982f9b6
Nidolai <11526011+Nidolai@users.noreply.github.com>
2022-09-07 20:54:55 +0200

    Added comments and moved helper functions to the top

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -2,6 +2,21 @@ import { useMDXComponent } from 'next-contentlayer/hooks';
 import { allPosts, Post } from 'contentlayer/generated'
 import BlogLayout from 'components/Blog/BlogLayout'
 
+// Generates `/blog/<Slug>`
+export async function getStaticPaths() {
+  return {
+    paths: allPosts.map((p) => ({ params: { slug: p.slug } })),
+    fallback: false
+  };
+}
+
+// Used to pass slugs to component
+export async function getStaticProps({ params }: any) {
+  const post = allPosts.find((post) => post.slug === params.slug);
+
+  return { props: { post } };
+}
+
 const PostPage = ({ post }: { post: Post }) => {
   const MDXContent = useMDXComponent(post.body.code)
 
@@ -13,16 +28,3 @@ const PostPage = ({ post }: { post: Post }) => {
 }
 
 export default PostPage
-
-export async function getStaticPaths() {
-  return {
-    paths: allPosts.map((p) => ({ params: { slug: p.slug } })),
-    fallback: false
-  };
-}
-
-export async function getStaticProps({ params }: any) {
-  const post = allPosts.find((post) => post.slug === params.slug);
-
-  return { props: { post } };
-}

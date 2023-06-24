a6f97b2e7aac91beb0870cb89fa48e0b1cbd6aff
Nidolai <11526011+Nidolai@users.noreply.github.com>
2023-06-24 11:17:22 +0200

    Updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Welcome from "components/Welcome";
+
 export default async function HomePage() {
     return (
         <section className="max-w-2xl w-full my-auto text-3xl text-center">
-            <h1>Nidolai</h1>
+            <Welcome />
         </section>
     )
 }
